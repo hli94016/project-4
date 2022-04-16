cdb73b00c3c7ad0b617f5c12fb74e0119d765cbf
Ryan <ryangapolonio@gmail.com>
2022-04-16 12:08:31 -0700

    Added more data

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Route to render index.html template using data from Mongo
+@app.route('/')
+def home():
+	return render_template('index.html')
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
